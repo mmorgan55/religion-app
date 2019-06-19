337b26c235382c7444bf9a81da9a58f9d6e5f2bf
Matthew Morgan <mmorgan55@cnm.edu>
2019-06-19 15:39:09 -0600

    Moved code for each button into its own class

--- lib/main.dart
+++ lib/main.dart
@@ -25,57 +25,52 @@ class AppState extends State<App> {
       padding: EdgeInsets.all(25),
       child: Column(
         children: <Widget>[
-          Flexible(
-            flex: 3,
-            fit: FlexFit.tight,
-            child: Container(
-              margin: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    blurRadius: 10.0,
-                    offset: Offset(0, 10),
-                  ),
-                ],
-              ),
-              child: Center(
-                child: Text(
-                  "TEST",
-                  style: TextStyle(
-                    fontFamily: "Georgia",
-                    fontSize: 50,
-                  ),
-                ),
-              ),
-            ),
+          HomePageButton(
+            2,
+            color: Colors.amberAccent,
+            str: "Hello",
           ),
-          Flexible(
-            flex: 1,
-            fit: FlexFit.tight,
-            child: Container(),
-          ),
-          Flexible(
-            flex: 3,
-            fit: FlexFit.tight,
-            child: Container(
-              margin: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Colors.redAccent,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black54,
-                    blurRadius: 10.0,
-                    offset: Offset(0, 10),
-                  ),
-                ],
-              ),
-            ),
-          )
+          HomePageButton(1)
         ],
+      ),
+    );
+  }
+}
+
+class HomePageButton extends StatelessWidget {
+  final int flexNum;
+  final Color color;
+  final String str;
+  HomePageButton(this.flexNum, {this.color: Colors.blueGrey, this.str: "BLANK"})
+      : assert(flexNum != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: flexNum,
+      fit: FlexFit.tight,
+      child: Container(
+        margin: EdgeInsets.all(7.5),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black54,
+              blurRadius: 10.0,
+              offset: Offset(0, 10),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            str,
+            style: TextStyle(
+              fontFamily: "Georgia",
+              fontSize: 50,
+            ),
+          ),
+        ),
       ),
     );
   }
